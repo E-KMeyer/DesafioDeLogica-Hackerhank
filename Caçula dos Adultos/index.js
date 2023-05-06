89b1cb4aa85cea9e3743a3c5fab3b19b860b13d4
Enio K S Meyer <enio.kaique@gmail.com>
2023-05-06 07:52:51 -0300

    Caçula dos Adultos

--- Caçula dos Adultos/index.js
+++ Caçula dos Adultos/index.js
@@ -17,3 +17,19 @@ function solucao(lista) {
 }
 
 solucao(Pessoas);
+
+
+/*
+(outra forma para resolução do exercícios)
+
+const autorizados = lista.filter(idade => idade >= 18); // array com as idades das pessoas autorizadas, usando o método filter para filtrar as idades maiores ou iguais a 18
+    if (autorizados.length === 0) { // se o array de autorizados estiver vazio
+        console.log('CRESCA E APARECA'); // imprimir uma mensagem
+    } else { // se o array de autorizados não estiver vazio
+        const menor = Math.min(...autorizados); // variável com a menor idade do array de autorizados, usando o método Math.min e o operador spread para obter o mínimo de um array
+        console.log(menor); // imprimir a menor idade
+    }
+
+    */
+
+    
